Fix alpha broadcasting; Fix scheduler

train_c2i.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torchvision.utils import save_image
-from einops import rearrange
+from einops import rearrange, repeat
 
 from models.ema import EMA
 from drifting import compute_drift_c2i
@@ -244,44 +244,44 @@ def main():
 
     def train_step(batch):
         # get data
-        x_real = batch["image"].float().to(device)                                      # (Ng * Nrpp, C, H, W)
-        y = batch["label"].long().to(device)                                            # (Ng * Nrpp, )
-        x_unc = batch["image_unc"][:Ng * Nupp].float().to(device)                       # (Ng * Nupp, C, H, W)
+        x_real = batch["image"].float().to(device)                 # (Ng * Nrpp, C, H, W)
+        y = batch["label"].long().to(device)                       # (Ng * Nrpp, )
+        x_unc = batch["image_unc"][:Ng * Nupp].float().to(device)  # (Ng * Nupp, C, H, W)
         # encode to latent
         if not args.use_latent_dataset:
             with torch.no_grad():
-                x_real = autoencoder.encode(x_real)                                     # (Ng * Nrpp, C, H, W)
-                x_unc = autoencoder.encode(x_unc)                                       # (Ng * Nupp, C, H, W)
+                x_real = autoencoder.encode(x_real)  # (Ng * Nrpp, C, H, W)
+                x_unc = autoencoder.encode(x_unc)    # (Ng * Nupp, C, H, W)
         # zero gradients
         optimizer.zero_grad()
         # forward
         with torch.autocast("cuda", dtype=torch.bfloat16, enabled=args.bf16):
             # generate fake samples
             z = torch.randn(Ng * Nfpp, *input_shape, device=device)
-            yc = y.reshape(Ng, Nrpp)[:, 0]                                              # (Ng, )
-            yc = yc.repeat_interleave(Nfpp)                                             # (Ng * Nfpp, )
-            alpha = sample_alpha(Ng)                                                    # (Ng, )
-            alpha = alpha.repeat_interleave(Nfpp)                                       # (Ng * Nfpp, )
-            x_fake = model(z, y=yc, alpha=alpha)                                        # (Ng * Nfpp, C, H, W)
+            yc = y.reshape(Ng, Nrpp)[:, 0]                            # (Ng, )
+            yc = yc.repeat_interleave(Nfpp)                           # (Ng * Nfpp, )
+            alpha = sample_alpha(Ng)                                  # (Ng, )
+            alpha = broadcast_tensor(alpha)                           # (Ng, )
+            alpha = alpha.repeat_interleave(Nfpp)                     # (Ng * Nfpp, )
+            x_fake = model(z, y=yc, alpha=alpha)                      # (Ng * Nfpp, C, H, W)
             # extract features
             with torch.no_grad():
-                feat_real = encoder(x_real, autoencoder=autoencoder)                    # dict of (B, Ng * Nrpp, D)
-                feat_unc = encoder(x_unc, autoencoder=autoencoder)                      # dict of (B, Ng * Nupp, D)
-            feat_fake = encoder(x_fake, autoencoder=autoencoder)                        # dict of (B, Ng * Nfpp, D)
+                feat_real = encoder(x_real, autoencoder=autoencoder)  # dict of (F, Ng * Nrpp, D)
+                feat_unc = encoder(x_unc, autoencoder=autoencoder)    # dict of (F, Ng * Nupp, D)
+            feat_fake = encoder(x_fake, autoencoder=autoencoder)      # dict of (F, Ng * Nfpp, D)
         # compute drifting field for each feature
         loss = torch.tensor(0.0, device=device)
         info = {}
         for name in feat_real.keys():
-            f_real = feat_real[name].float()                                            # (B, Ng * Nrpp, D)
-            f_unc = feat_unc[name].float()                                              # (B, Ng * Nupp, D)
-            f_fake = feat_fake[name].float()                                            # (B, Ng * Nfpp, D)
+            f_real = feat_real[name].float()  # (F, Ng * Nrpp, D)
+            f_unc = feat_unc[name].float()    # (F, Ng * Nupp, D)
+            f_fake = feat_fake[name].float()  # (F, Ng * Nfpp, D)
             # move the group dimension to the feature dimension
             # drifting field is computed independently for each group
-            B = f_real.shape[0]
-            f_real = rearrange(f_real, "b (ng nr) d -> (b ng) nr d", ng=Ng, nr=Nrpp)    # (B * Ng, Nrpp, D)
-            f_unc = rearrange(f_unc, "b (ng nr) d -> (b ng) nr d", ng=Ng, nr=Nupp)      # (B * Ng, Nupp, D)
-            f_fake = rearrange(f_fake, "b (ng nf) d -> (b ng) nf d", ng=Ng, nf=Nfpp)    # (B * Ng, Nfpp, D)
-            alpha_reshape = alpha.unsqueeze(0).repeat(B, 1).reshape(B * Ng, Nfpp)       # (B * Ng, Nfpp)
+            alpha_reshape = repeat(alpha, "(ng nf) -> (f ng) nf", f=f_real.shape[0], ng=Ng)  # (F * Ng, Nfpp)
+            f_real = rearrange(f_real, "f (ng nr) d -> (f ng) nr d", ng=Ng, nr=Nrpp)         # (F * Ng, Nrpp, D)
+            f_unc = rearrange(f_unc, "f (ng nr) d -> (f ng) nr d", ng=Ng, nr=Nupp)           # (F * Ng, Nupp, D)
+            f_fake = rearrange(f_fake, "f (ng nf) d -> (f ng) nf d", ng=Ng, nf=Nfpp)         # (F * Ng, Nfpp, D)
             with torch.no_grad():
                 V, _info = compute_drift_c2i(
                     x_real=f_real.detach(),

train_unc.py
@@ -226,28 +226,28 @@ def main():
         # encode to latent
         if not args.use_latent_dataset:
             with torch.no_grad():
-                x_real = autoencoder.encode(x_real)                                     # (Ng * Nrpp, C, H, W)
+                x_real = autoencoder.encode(x_real)  # (Ng * Nrpp, C, H, W)
         # zero gradients
         optimizer.zero_grad()
         # forward
         with torch.autocast("cuda", dtype=torch.bfloat16, enabled=args.bf16):
             # generate fake samples
             z = torch.randn(Ng * Nfpp, *input_shape, device=device)
-            x_fake = model(z)                                                           # (Ng * Nfpp, C, H, W)
+            x_fake = model(z)  # (Ng * Nfpp, C, H, W)
             # extract features
             with torch.no_grad():
-                feat_real = encoder(x_real, autoencoder=autoencoder)                    # dict of (B, Ng * Nrpp, D)
-            feat_fake = encoder(x_fake, autoencoder=autoencoder)                        # dict of (B, Ng * Nfpp, D)
+                feat_real = encoder(x_real, autoencoder=autoencoder)  # dict of (F, Ng * Nrpp, D)
+            feat_fake = encoder(x_fake, autoencoder=autoencoder)      # dict of (F, Ng * Nfpp, D)
         # compute drifting field for each feature
         loss = torch.tensor(0.0, device=device)
         info = {}
         for name in feat_real.keys():
-            f_real = feat_real[name].float()                                            # (B, Ng * Nrpp, D)
-            f_fake = feat_fake[name].float()                                            # (B, Ng * Nfpp, D)
+            f_real = feat_real[name].float()  # (F, Ng * Nrpp, D)
+            f_fake = feat_fake[name].float()  # (F, Ng * Nfpp, D)
             # move the group dimension to the feature dimension
             # drifting field is computed independently for each group
-            f_real = rearrange(f_real, "b (ng nr) d -> (b ng) nr d", ng=Ng, nr=Nrpp)    # (B * Ng, Nrpp, D)
-            f_fake = rearrange(f_fake, "b (ng nf) d -> (b ng) nf d", ng=Ng, nf=Nfpp)    # (B * Ng, Nfpp, D)
+            f_real = rearrange(f_real, "f (ng nr) d -> (f ng) nr d", ng=Ng, nr=Nrpp)    # (F * Ng, Nrpp, D)
+            f_fake = rearrange(f_fake, "f (ng nf) d -> (f ng) nf d", ng=Ng, nf=Nfpp)    # (F * Ng, Nfpp, D)
             with torch.no_grad():
                 V, _info = compute_drift(
                     x_real=f_real.detach(),